4afa911e6c48119a4e48c997ae7df60660a24174
Malujoro <mateus_rocha_sousa@hotmail.com>
2024-09-18 17:13:17 -0300

    ajuste da struct

--- arvore_matricula/arvore_matricula.h
+++ arvore_matricula/arvore_matricula.h
@@ -5,21 +5,13 @@ typedef struct no_matricula
     struct no_matricula *direito;
 } No_Matricula;
 
-typedef struct arvore_matricula
-{
-    No_Matricula *raiz;
-} Arvore_Matricula;
-
 No_Matricula *no_matricula_cria(int matricula);
+No_Matricula *arvore_matricula_cria();
+void arvore_matricula_desaloca(No_Matricula **raiz);
 
-Arvore_Matricula *arvore_matricula_cria();
-
-void no_matricula_desaloca(No_Matricula **no);
-
-void arvore_matricula_desaloca(Arvore_Matricula **arvore_matricula);
-
-void arvore_matricula_add(Arvore_Matricula *arvore_matricula, int matricula);
+void arvore_matricula_add(No_Matricula **raiz, int matricula);
 
 void arvore_matricula_exibir(No_Matricula *raiz);
 
-No_Matricula *arvore_matricula_remover(Arvore_Matricula *arvore, int matricula);
+No_Matricula *arvore_matricula_remover_no(No_Matricula **raiz);
+No_Matricula *arvore_matricula_remover(No_Matricula **raiz, int matricula);
